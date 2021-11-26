22f12422a587a47f7d9a2b5eeb6b73319b197ccc
Zachary Robinson <zrobins2@swarthmore.edu>
2021-11-26 03:12:17 -0500

    Fetch user data from LDAP
    Instead of serializing the entire user/session object into a cookie

--- src/auth.ts
+++ src/auth.ts
@@ -4,6 +4,7 @@ import LdapStrategy from 'passport-ldapauth'
 import session from 'express-session'
 
 import { logger } from './logging'
+import { LDAP_CONFIG, ldapClient } from './ldap'
 import { Handler } from 'express'
 
 export const isLoggedIn: Handler = (req, res, next) => {
@@ -21,11 +22,7 @@ export const isLoggedIn: Handler = (req, res, next) => {
 
 export const configureAuth = (app: any): void => {
     passport.use(new LdapStrategy({
-        server: {
-            url: process.env.LDAP_URL,
-            searchBase: process.env.LDAP_SEARCH_BASE,
-            searchFilter: "(uid={{username}})"
-        }
+        server: LDAP_CONFIG
     }))
     app.use(session({
         secret: 'beans',
@@ -40,11 +37,45 @@ export const configureAuth = (app: any): void => {
 
     passport.serializeUser((user: any, done) => {
         logger.debug(`Serializing user ${user['uid']}`)
-        done(null, user)
+        done(null, user.uid)
     })
 
-    passport.deserializeUser((user: any, done) => {
-        logger.debug(`Deserializing user ${user['uid']}`)
-        done(null, user)
+    passport.deserializeUser(async (uid: string, done) => {
+        logger.debug(`Deserializing user ${uid}`)
+        try {
+            const user = await new Promise((resolve, reject) => {
+                ldapClient.search(process.env.LDAP_SEARCH_BASE, { filter: `(uid=${uid})`, scope: 'sub' },
+                    (err, res) => {
+                        let found = false
+                        if (err) {
+                            logger.error(`LDAP search error in deserialization: ${err}`)
+                            reject(err)
+                        }
+                        res.on('error', (err) => {
+                            logger.error(`LDAP search error in deserialization: ${err}`)
+                            reject(err)
+                        });
+                        res.on('searchEntry', (entry) => {
+                            found = true
+                            resolve(entry.object)
+                        });
+                        res.on('end', (result) => {
+                            if (!found) {
+                                resolve(null)
+                            }
+                        });
+                    })
+            })
+            if (user) {
+                logger.debug(`Found LDAP entry for ${uid}`)
+                done(null, user)
+            } else {
+                logger.warn(`Could not find LDAP entry for ${uid}`)
+                done(null, user)
+            }
+        } catch (err) {
+            done(err)
+        }
     })
+
 }
