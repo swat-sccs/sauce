4ec34a391b49c2197b5e5b359d52a4641a4e574b
Zachary Robinson <zrobins2@swarthmore.edu>
2021-12-01 20:13:59 -0500

    Put admin flag on the user object
    Eventually we'll be passing the user object into all the templates to do
    rendery things and this benefits that; we can e.g. display the
    link to the admin dashboard only to admins

--- src/auth.ts
+++ src/auth.ts
@@ -3,7 +3,7 @@ import LdapStrategy from 'passport-ldapauth';
 import session from 'express-session';
 
 import { logger } from './logging';
-import { LDAP_CONFIG, ldapClient, searchAsyncUid } from './ldap';
+import { LDAP_CONFIG, ldapClient, searchAsyncUid, searchAsync } from './ldap';
 import { Handler } from 'express';
 import MongoStore from 'connect-mongo';
 
@@ -49,9 +49,28 @@ export const configureAuth = (app: any): void => {
   passport.deserializeUser(async (uid: string, done) => {
     logger.debug(`Deserializing user ${uid}`);
     try {
-      const user = await searchAsyncUid(ldapClient, uid);
+      const [user, admins] = await Promise.all([
+        searchAsyncUid(ldapClient, uid),
+        // is there a better way to do this than getting the entire list of staff members
+        // and if there isn't, can we cache this or something
+        searchAsync(
+          ldapClient,
+          `(&(cn=${process.env.LDAP_ADMIN_GROUP}))`,
+          process.env.LDAP_SEARCH_BASE_GROUP,
+        )
+          .then((value) => value.memberUid)
+          .catch((err) => {
+            // if there's an error getting admins, log it but we shouldn't explode if LDAP doesn't
+            // want to give us the admin list
+            logger.error('LDAP error getting admin list:', err);
+            return [];
+          }),
+      ]);
       if (user) {
         logger.debug(`Found LDAP entry for ${uid}`);
+
+        user.admin = admins.includes(user.uid);
+
         done(null, user);
       } else {
         logger.warn(`Could not find LDAP entry for ${uid}`);
--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -11,23 +11,11 @@ const isAdmin: Handler = async (req: any, res, next) => {
   // that is, check if logged in and if so we'll run the inner bit
   isLoggedIn(req, res, async () => {
     if (req.user) {
-      // is there a better way to do this than getting the entire list of staff members
-      // and if there isn't, can we cache this or something
-      const admins = (
-        await searchAsync(
-          ldapClient,
-          `(&(cn=${process.env.LDAP_ADMIN_GROUP}))`,
-          process.env.LDAP_SEARCH_BASE_GROUP,
-        )
-      ).memberUid;
-
-      if (admins.includes(req.user.uid)) {
-        logger.debug(`${req.user.uid} is a member of ${process.env.LDAP_ADMIN_GROUP}`);
+      if (req.user.admin) {
+        logger.debug(`${req.user.uid} is an admin`);
         next();
       } else {
-        logger.debug(
-          `${req.user.uid} is not in ${process.env.LDAP_ADMIN_GROUP} and is unauthorized`,
-        );
+        logger.warn(`${req.user.uid} is not an admin and is unauthorized`);
         // TODO nicer 403 page
         res.status(403).send('You must be an administrator to access this page!');
       }
