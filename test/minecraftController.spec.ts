67f6093acb9784b512286c0ee35d424fe3241204
Zachary Robinson <zrobins2@swarthmore.edu>
2022-02-05 19:39:17 -0500

    Test github workflows

--- test/minecraftController.spec.ts
+++ test/minecraftController.spec.ts
@@ -11,6 +11,7 @@ import * as minecraftApi from '../src/integration/minecraftApi';
 import { MinecraftWhitelistModel } from '../src/integration/models';
 import { initMongo } from '../src/integration/mongo';
 
+//
 describe('minecraftController', function () {
   let sandbox: SinonSandbox;
   let expect: Chai.ExpectStatic;
