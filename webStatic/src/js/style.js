4d82958363b9314e9ebcf1669716cf81dc3bdf8c
Ark Lu <39932933+qiyoulu@users.noreply.github.com>
2022-09-18 00:18:06 -0400

    Update style.js

--- webStatic/src/js/style.js
+++ webStatic/src/js/style.js
@@ -7,3 +7,22 @@ if (prefersDarkScheme.matches) {
 } else {
   document.body.classList.remove("dark-theme");
 }
+
+// Select the button
+const btn = document.querySelector(".btn-toggle");
+ 
+// Listen for a click on the button 
+btn.addEventListener("click", function() {
+  // Toggle the .dark-theme class on the body
+  document.body.classList.toggle("dark-theme");
+  
+  // Let's say the theme is equal to light
+  let theme = "light";
+  // If the body contains the .dark-theme class...
+  if (document.body.classList.contains("dark-theme")) {
+    // ...then let's make the theme dark
+    theme = "dark";
+  }
+  // Then save the choice in a cookie
+  document.cookie = "theme=" + theme;
+});
