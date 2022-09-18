d196863a9200ed7d508d4375a72bd6174973152c
Ark Lu <39932933+qiyoulu@users.noreply.github.com>
2022-09-18 00:05:14 -0400

    Update style.js

--- webStatic/src/js/style.js
+++ webStatic/src/js/style.js
@@ -1,1 +1,9 @@
 import '../scss/sauce.scss';
+
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+if (prefersDarkScheme.matches) {
+  document.body.classList.add("dark-theme");
+} else {
+  document.body.classList.remove("dark-theme");
+}
