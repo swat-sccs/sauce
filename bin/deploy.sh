d674a2bc28189560809936d883f83253f65a0213
Thomas Makin <halorocker89@gmail.com>
2025-06-02 15:47:02 -0400

    deploy: add forced service update
    Hacky but rn we manually build and deploy with `latest` tag

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,3 +3,4 @@
 echo "Deploying Stack"
 docker compose -f docker-compose.yml -f docker-compose.override.yml build
 docker stack deploy -c docker-compose.yml -c docker-compose.override.yml sauce
+docker service update --force sauce_sauce
