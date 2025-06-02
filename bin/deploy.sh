4b2612765c7e3e10069592adb4fa44ac644b4881
Thomas Makin <halorocker89@gmail.com>
2025-06-02 13:58:42 -0400

    deploy: use stack deploy instead of compose

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Deploying Stack"
-docker compose -f docker-compose.yml build
+docker compose -f docker-compose.yml -f docker-compose.override.yml build
 docker stack deploy -c docker-compose.yml -c docker-compose.override.yml sauce
